91e47484cc631b3109195c1ed0530486a93e5833
QAFed <143259597+QAFed@users.noreply.github.com>
2024-09-08 12:14:29 +0300

    структура и проба запуска

---
+++ pages/start_page.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+class HomePageSamokat:
+    logo_yandex = [By.XPATH, '//a[contains(@class, "Header_LogoYandex")]']
+    button_order_in_header = [By.XPATH, '//div[contains(@class, "Header_Nav")]/button[text()="Заказать"]']
+    button_order_in_body = [By.XPATH, '//div[contains(@class, "Home_FinishButton")]/button[text()="Заказать"]']
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def wait_for_load_logo(self):
+        WebDriverWait(self.driver,5).until(expected_conditions.visibility_of_element_located(self.logo_yandex))
+
+    def click_button_order_in_header(self):
+        self.driver.find_element(*self.button_order_in_header).click()
+
+    def click_button_order_in_body(self):
+        self.driver.find_element(*self.button_order_in_body).click()
+
