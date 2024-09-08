0dcc39d23523eb49ee07033028e959489d1334bd
QAFed <143259597+QAFed@users.noreply.github.com>
2024-09-08 17:01:01 +0300

    урезал списки

--- tests/test_start_page_questions.py
+++ tests/test_start_page_questions.py
@@ -17,15 +17,15 @@ class TestStartPage:
         home_page.wait_for_load_logo()
         home_page.check_answer_by_question(quest, answer)
 
-    @pytest.mark.parametrize('quest, answer', QestAndAnswers.div_list)
-    def test_answers_is_hidden_by_default(self, quest, answer):
+    @pytest.mark.parametrize('quest', [x[0]for x in QestAndAnswers.div_list])
+    def test_answers_is_hidden_by_default(self, quest):
         self.driver.get('https://qa-scooter.praktikum-services.ru/')
         home_page = HomePageSamokat(self.driver)
         home_page.wait_for_load_logo()
         home_page.check_answers_is_hidden(quest)
 
-    @pytest.mark.parametrize('quest, answer', QestAndAnswers.div_list)
-    def test_answer_not_hidden_after_click(self, quest, answer):
+    @pytest.mark.parametrize('quest',  [x[0]for x in QestAndAnswers.div_list])
+    def test_answer_not_hidden_after_click(self, quest):
         self.driver.get('https://qa-scooter.praktikum-services.ru/')
         home_page = HomePageSamokat(self.driver)
         home_page.wait_for_load_logo()
